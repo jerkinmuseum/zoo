a1229949a0e9510a10f2d5408cef084fb4eed7f7
Flavius <flaav.gbr@gmail.com>
2015-08-15 17:25:14 +0300

    Update controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,9 +2,7 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('AnimalsCtrl', function($scope) {})
-
-.controller('ChatsCtrl', function($scope, Chats) {
+.controller('AnimalsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -13,14 +11,14 @@ angular.module('starter.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
+  $scope.animals = Animals.all();
+  $scope.remove = function(animal) {
+    Animals.remove(animal);
   };
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+.controller('AnimalDetailCtrl', function($scope, $stateParams, Animals) {
+  $scope.animal = Animals.get($stateParams.animalId);
 })
 
 .controller('MapCtrl', function($scope) {
